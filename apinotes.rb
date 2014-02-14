f76c96cdc908229a650e0bf856087c5f273cdbca
Emily Corro <emilyrebeca20@gmail.com>
2014-02-14 03:14:07 -0430

    API terminada

--- apinotes.rb
+++ apinotes.rb
@@ -10,18 +10,24 @@ ActiveRecord::Base.establish_connection(
 :host => "localhost",  
 :database => "TASKAPP",
 :username => "root",
-:password => "",
+:password => "20191602",
 )  
-
+#No autorizado
 class User < ActiveRecord::Base
 end
 
+class Task < ActiveRecord::Base
+end 
+
 class ReturnCode
 	CANEXEC =	1000	#Ejecutar la tarea
-	UNAUTH = 	1001	#No autorizado
-	LOGINREQ = 	1002	#Necesario login
-	RNOTFOUND = 1003	#Recurso no encontrado
-	BADLOGIN = 	1004	#Usuario o password inválido
+	GENEVENT = 	1001	#Evento general
+	GENEXCEPT =	1002	#Excepcion general
+	LOGINREQ = 	1003	#Necesario login
+	RNOTFOUND = 1004	#Recurso no encontrado
+	BADLOGIN = 	1005	#Usuario o password inválido
+	UNAUTH = 	1006	#Usuario no autorizado
+	ADMINREQ = 	1007	#Necesario ser administrador
 end
 
 class AccessResult
@@ -37,7 +43,7 @@ end
 
 #Recibe una solicitud devuelve un objeto de
 #tipo User
-def userlogin()
+def userlogged()
 	basica = request.env["HTTP_AUTHORIZATION"].to_s.split("Basic ")
 	basicas = basica[1].to_s
 	authdata = (Base64.decode64(basica.to_s)).split(':')
@@ -57,7 +63,7 @@ def userAuth(adminreq=false,loginreq=false,userid=nil)
 		authdata = (Base64.decode64(basica.to_s)).split(':')
 		username = authdata[0].to_s
 		pass = authdata[1].to_s
-		if not username.length or not pass.length then									#Si no se indica usuario o contraseña
+		if username.length == 0 or pass.length == 0 then									#Si no se indica usuario o contraseña
 			response.code = ReturnCode::LOGINREQ
 			response.message = "Debe iniciar sesión."
 			return response
@@ -69,16 +75,13 @@ def userAuth(adminreq=false,loginreq=false,userid=nil)
 			return response
 		end
 		if adminreq and userlog.role != "admin" then									#Si se requiere ser administrador y no lo es
-			response.code = ReturnCode::UNAUTH
-			response.message = "Usuario no autorizado"
+			response.code = ReturnCode::ADMINREQ
+			response.message = "No es administrador"
 			return response
 		end
-		if (userlog.role != "admin") and (userid.to_s != userlog.id.to_s) then
-				#puts userlog.role
-				#puts userlog.id
-				#puts userid
+		if (userlog.role != "admin") and (userid.to_s != userlog.id.to_s) then			#Si se quiere hacer una consulta de otro usuario
 				response.code = ReturnCode::UNAUTH
-				response.message = "Usuario no autorizado 2"
+				response.message = "Usuario no autorizado"
 				return response
 		end
 	end
@@ -89,6 +92,10 @@ get '/' do
 	"Hello World"
 end
 
+get "/time" do
+	Time.now.to_s
+end
+
 #Todos los usuarios
 get '/apinotes/user' do
 	res = userAuth(adminreq=true,loginreq=true)
@@ -96,124 +103,105 @@ get '/apinotes/user' do
 		if User.all.length > 0 then			#Si hay algun usuario registrado
 			User.all.to_json				#Se devuelven en formato to_json
 		else  								#Si no hay usuario registrados
-			ar = AccessResult.new(ReturnCode::RNOTFOUND,"No hay usuarios registrados")
-			ar.to_json						#Se devuelve el error
+			AccessResult.new(ReturnCode::RNOTFOUND,"No hay usuarios registrados").to_json
 		end
 	else
 		res.to_json
 	end 
-
-	# u = userlogin(request)					#Se verifica usuario y contrasena
-	# if u then									#Si existe
-	# 	if u.role == 'admin' then		#Tiene que ser administrador
-	# 		if User.all.length > 0 then			#Si hay algun usuario registrado
-	# 			User.all.to_json				#Se devuelven en formato to_json
-	# 		else  								#Si no hay usuario registrados
-	# 			"No hay usuarios registrados"	#Se devuelve el error
-	# 		end
-	# 	else 									#Si no es administrador
-	# 		"No esta autorizado"				#Se devuelve el error
-	# 	end
-	# else 										#Si el usuario no existe
-	# 	"Debe iniciar sesion"					#Se devuelve el error
-	# end
 end
 
 #Un usuario
-get '/apinotes/user/:id' do
-	res = userAuth(adminreq=false,loginreq=true,userid=params[:id])	
+get '/apinotes/user/:idu' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
 	if res.code == ReturnCode::CANEXEC then
-		u = User.where(id:params[:id]).first
-		u.to_json
+		u = User.where(id:params[:idu]).first
+		if u then
+			u.to_json
+		else
+			AccessResult.new(ReturnCode::GENEVENT,"El usuario no existe").to_json
+		end
+	elsif res.code == ReturnCode::LOGINREQ then
+		u = User.where(id:params[:idu]).first
+		if u then 
+			AccessResult.new(ReturnCode::GENEVENT,u.question).to_json
+		else
+			AccessResult.new(ReturnCode::RNOTFOUND,"No existe el usuario").to_json
+		end
 	else
 		res.to_json
 	end 
-
-	# if ulog then								#Si existe
-	# u = User.where(id:params[:id]).first	#Se obtiene el usuario requerido en la peticion
-	# 	if u then								#Si este existe
-	# 											#Se compara el id del login con el de la peticion
-	# 		if ulog.id == u.id || ulog.role == 'admin' then				
-	# 			u.to_json						#Si coincide, se devuelve en formato json
-	# 		else 								#Si no coincide
-	# 			"No esta autorizado"			#Se devuelve el error	 
-	# 		end
-	# 	else 									#Si el usuario buscado no existe
-	# 		"El usuario no existe"				#Se devuelve el error
-	# 	end
-	# else 										#Si el usuario no existe
-	# 	"Debe iniciar sesion"					#Se devuelve el error
-	# end
 end
 
 #Crear un usuario
-#Verificar que el email no sea duplicado!!!
 post '/apinotes/user' do
 	name = params[:name]
-	lastname = params[:lastn]
+	lastname = params[:lastname]
 	email = params[:email]
-	password = params[:pass]
-	question = params[:secquest]
-	answer = params[:secans]
+	password = params[:password]
+	question = params[:question]
+	answer = params[:answer]
 
-	u = User.new
-	u.name = name
-	u.last_name = lastname
-	u.email = email
-	u.question = question
-	u.answer = answer
-	u.password = password
-	u.role = 'user'
-	u.save
-
-	# puts name 
-	# puts lastname 
-	# puts email 
-	# puts answer 
-	# puts password
-
-	"Usuario creado"
+	if name and lastname and email and password and question and answer then
+		du = User.where(email:email).first
+		if not du then
+			u = User.new
+			u.name = name
+			u.last_name = lastname
+			u.email = email
+			u.question = question
+			u.answer = answer
+			u.password = password
+			u.role = 'user'
+			u.save
+			AccessResult.new(ReturnCode::GENEVENT,"Usuario creado").to_json
+		else
+			AccessResult.new(ReturnCode::GENEXCEPT,"El usuario ya existe").to_json
+		end
+	else
+		AccessResult.new(ReturnCode::GENEXCEPT,"Faltan parametros").to_json
+	end
 end
 
 #Actualizar un usuario
-#Para agendar actualizar las fechas
-#Para recuperar password verificar respuesta de seguridad
-#y reemplazar la contraseña
-put '/apinotes/user/:id' do
-	res = userAuth(adminreq=false,loginreq=true,userid=params[:id])	
-	u = User.where(id:params[:id]).first
-	if res.code == ReturnCode::CANEXEC then			
-		name = params[:name]
-		lastname = params[:lastname]
-		email = params[:email]
-		password = params[:password]
-		question = params[:question]
-		answer = params[:answer]
-		if name then
-			u.update(name:name)
-			# u.nombre = name
+put '/apinotes/user/:idu' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then	
+		u = User.where(id:params[:idu]).first
+		if u then		
+			name = params[:name]
+			lastname = params[:lastname]
+			email = params[:email]
+			password = params[:password]
+			question = params[:question]
+			answer = params[:answer]
+			if name then
+				u.update(name:name)
+				# u.nombre = name
+			end
+			if lastname then
+				u.update(last_name:lastname)
+				# u.apellido = lastname
+			end
+			if email then
+				u.update(email:email)
+				# u.email = email
+			end
+			if question then
+				u.update(question:question)
+				# u.respuesta_pregunta = answer
+			end
+			if answer then
+				u.update(answer:answer)
+				# u.respuesta_pregunta = answer
+			end
+			if password then
+				u.update(password:password)
+				# u.contrasena = password
+			end
+			AccessResult.new(ReturnCode::GENEVENT,"Usuario actualizado").to_json
+		else
+			AccessResult.new(ReturnCode::RNOTFOUND,"El usuario no existe").to_json
 		end
-		if lastname then
-			u.update(last_name:lastname)
-			# u.apellido = lastname
-		end
-		if email then
-			u.update(email:email)
-			# u.email = email
-		end
-		if question then
-			u.update(question:question)
-			# u.respuesta_pregunta = answer
-		end
-		if answer then
-			u.update(answer:answer)
-			# u.respuesta_pregunta = answer
-		end
-		if password then
-			u.update(password:password)
-			# u.contrasena = password
-		end
-		"Usuario actualizado"			#Se actualiza el usuario
 	else
 		res.to_json
 	end 						
@@ -221,40 +209,167 @@ end
 
 #Borrar todos los usuarios
 delete '/apinotes/user' do
-	u = userlogin(request)						#Se verifica usuario y contrasena
-	if u then									#Si existe
-		if u.role == 'admin' then		#Tiene que ser administrador
-			if User.all.length > 0 then			#Si hay algun usuario registrado
-				User.delete_all					#Se eliminan todos
-				"Todos los usuarios fueron eliminados"
-			else  								#Si no hay usuario registrados
-				"No hay usuarios registrados"	#Se devuelve el error
-			end
-		else 									#Si no es administrador
-			"No esta autorizado"				#Se devuelve el error
+	res = userAuth(adminreq=true,loginreq=true)	
+	if res.code == ReturnCode::CANEXEC then
+		if User.where(role:'user').length > 0 then			#Si hay algun usuario registrado
+			User.delete_all("role = 'user'")					#Se eliminan todos
+			AccessResult.new(ReturnCode::GENEVENT,"Todos los usuarios fueron eliminados").to_json
+		else  								#Si no hay usuario registrados
+			AccessResult.new(ReturnCode::RNOTFOUND,"No hay usuarios registrados").to_json
 		end
-	else 										#Si el usuario no existe
-		"Debe iniciar sesion"					#Se devuelve el error
+	else
+		res.to_json
 	end
 end
 
 #Borrar un usuario
-delete '/apinotes/user/:id' do
-	ulog = userlogin(request)					#Se verifica usuario y contrasena
-	if ulog then								#Si existe
-		u = User.where(id:params[:id]).first	#Se obtiene el usuario requerido en la peticion
-		if u then								#Si este existe
-												#Se compara el id del login con el de la peticion
-			if ulog.id == u.id || ulog.role == 'admin' then				
-				u.destroy
-				"Usuario eliminado"
-			else 								#Si no coincide
-				"No esta autorizado"			#Se devuelve el error	 
-			end
-		else 									#Si el usuario buscado no existe
-			"El usuario no existe"				#Se devuelve el error
+delete '/apinotes/user/:idu' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		u = User.where(id:params[:idu]).first	#Se obtiene el usuario requerido en la peticion			
+		if u then
+			u.delete
+			AccessResult.new(ReturnCode::GENEVENT,"Usuario eliminado").to_json
+		else 
+			AccessResult.new(ReturnCode::RNOTFOUND,"No existe el usuario").to_json
 		end
-	else 										#Si el usuario no existe
-		"Debe iniciar sesion"					#Se devuelve el error
+	else
+		res.to_json
+	end
+end
+
+#Todas las notas de un usuario
+get '/apinotes/user/:idu/note' do
+	res = userAuth(adminreq=false,loginreq=true,params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		tasks = Task.where(user_id:params[:idu])
+		if  tasks.length > 0 then
+			tasks.to_json
+		else
+			AccessResult.new(ReturnCode::RNOTFOUND,"No hay notas").to_json
+		end
+	else
+		res.to_json
+	end
+end
+
+#Detalles nota
+get '/apinotes/user/:idu/note/:idn' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		task  = Task.where(user_id:params[:idu],id:params[:idn]).take
+		if task then
+			task.to_json
+		else
+			AccessResult.new(ReturnCode::RNOTFOUND,"La nota no existe").to_json
+		end
+	else
+		res.to_json	
+	end
+end
+
+#Crear nota
+post '/apinotes/user/:idu/note' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])
+	if res.code == ReturnCode::CANEXEC then
+		u = User.where(id:params[:idu]).take
+		if u then
+			description = plan_date = first_plan_date = priority = type = nil
+			description = params[:description]
+			#creation_date = params[:creation_date]
+			creation_date = Time.now.strftime("%Y-%m-%d %I:%M:%S")
+			plan_date =  params[:plan_date]
+			if plan_date then
+				first_plan_date = plan_date
+			end
+			priority = params[:priority]
+			finished = 0 
+			type = params[:type]
+			user_id = params[:idu]
+			if description then
+				task = Task.new
+				task.description = description
+				task.creation_date = creation_date
+				task.plan_date = plan_date
+				task.first_plan_date = first_plan_date
+				task.priority = priority
+				task.finished = finished
+				task.ntype = type
+				task.user_id = user_id
+				task.save
+				AccessResult.new(ReturnCode::GENEVENT,"Nota creada").to_json
+			else
+				AccessResult.new(ReturnCode::GENEXCEPT,"Faltan parametros").to_json
+			end
+		else
+			AccessResult.new(ReturnCode::GENEXCEPT,"No existe el usuario").to_json
+		end
+	else
+		res.to_json
+	end
+end
+
+#Actualizar nota
+put '/apinotes/user/:idu/note/:idn'  do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		task  = Task.where(user_id:params[:idu],id:params[:idn]).take
+		if task then
+			description = params[:description]
+			plan_date =  params[:plan_date]
+			priority = params[:priority]
+			finished = params[:finished] 
+			type = params[:type]
+			if description then
+				task.update(description:description)
+			end
+			if plan_date then
+				task.update(plan_date:plan_date)
+				if not task.first_plan_date.to_s.length then
+					task.update(first_plan_date:plan_date)
+				end
+			end
+			if priority then
+				task.update(priority:priority)
+			end
+			if finished then
+				task.update(finished:finished)
+			end
+			if type then
+				task.update(ntype:type)
+			end
+			AccessResult.new(ReturnCode::GENEVENT,"Nota actualizada").to_json
+		else
+			AccessResult.new(ReturnCode::RNOTFOUND,"No existe la nota").to_json
+		end
+	else
+		res.to_json
+	end
+end
+
+#Eliminar todas las notas de un usuario
+delete '/apinotes/user/:idu/note' do	
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		Task.where(user_id:params[:idu]).delete_all
+		AccessResult.new(ReturnCode::GENEVENT,"Todas las notas han sido elminadas").to_json
+	else
+		res.to_json
+	end
+end
+
+#Eliminar nota
+delete '/apinotes/user/:idu/note/:idn' do
+	res = userAuth(adminreq=false,loginreq=true,userid=params[:idu])	
+	if res.code == ReturnCode::CANEXEC then
+		task  = Task.where(user_id:params[:idu],id:params[:idn]).take
+		if task then
+			task.delete
+			AccessResult.new(ReturnCode::GENEVENT,"Nota elminada").to_json
+		else
+			AccessResult.new(ReturnCode::GENEXCEPT,"Nota no encontrada").to_json
+		end
+	else
+		res.to_json
 	end
 end
--- scriptATI.sql
+++ scriptATI.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE TASKAPP;
 USE TASKAPP;
-CREATE TABLE tasks(id int primary key not null auto_increment, description char(50), creation_date date, plan_date date, first_plan_date date, priority int, finished bool, type char(50), user_id int );
-CREATE TABLE users(id int primary key not null auto_increment, name char(50), last_name char(50), email char(50), password char(50), question char(50), answer char(100), role char(30));
+CREATE TABLE tasks(id int primary key not null auto_increment, description varchar(500), creation_date datetime, plan_date datetime, first_plan_datetime datetime, priority int, finished bool, ntype varchar(100), user_id int );
+CREATE TABLE users(id int primary key not null auto_increment, name varchar(50), last_name varchar(50), email varchar(50), password varchar(50), question varchar(100), answer varchar(100), role varchar(30));
 
 ALTER TABLE tasks ADD CONSTRAINT FK_User FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
